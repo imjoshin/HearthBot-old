e4d6a4b7f0669fc114c9edb67f567d0f639a434a
Sharky! <jdeurt@gmail.com>
2018-04-19 20:28:02 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,8 +42,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 	if (config.ALLOW_CARDS) {
 		var cards = message.match(/\[\[(.*?)\]\]/g);
 		if (cards && cards.length) {
-			var parseCard = require("./lib/card");
-			parseCard(cards, user, userID, channelID, config, fetch);
+			var doCard = require("./lib/card");
+			doCard(cards, config, fetch, user, userID, channelID, message, evt);
 		}
 	}
 
@@ -51,43 +51,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		var decks = message.match(/AAE(.*?)(=|$)/g);
 
 		if (decks && decks.length) {
-			// limit number of decks
-			decks = decks.slice(0, config.DECK_LIMIT);
-
-			decks.forEach(function(deck) {
-				try {
-					decoded = deckstrings.decode(deck);
-				} catch (err){
-					logger.error("Error decoding deck string: '" + deck + "'");
-					return;
-				}
-
-				// get list of card ids
-				ids = [];
-				decoded['cards'].forEach(function(card) {
-					ids.push(card[0]);
-				});
-				decoded['heroes'].forEach(function(hero) {
-					ids.push(hero);
-				});
-
-				// get card data
-				fetch(config.API_URL + "id=" + ids.join(',') + "&key=" + auth.KEY, {method: 'GET'})
-				.then(function(response) {
-					return response.json();
-				})
-				.then(function(cardData) {
-					userInfo = "&u=" + user + "&uid=" + userID + "&cid=" + channelID;
-					if (!("error" in cardData)) {
-						bot.sendMessage({
-							to: channelID,
-							embed: formatDeck(decoded, cardData, deck, userInfo)
-						});
-					} else {
-						logger.error(cardData['error']);
-					}
-				});
-			});
+			var doDeck = require("./lib.deck");
+			doDeck(cards, config, fetch, user, userID, channelID, message, evt);
 		}
 	}
 });
